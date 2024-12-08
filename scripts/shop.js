c621af52bece8e9b586638758559f7f50f007c95
GamingDimiGD <gamingdimigd@gmail.com>
2024-12-08 18:01:51 +0800

    嘔我的天那個爛ㄟ哀搞出bug

--- scripts/shop.js
+++ scripts/shop.js
@@ -395,7 +395,7 @@ let customBGIMG = new ShopItem(
             });
             rb.addEventListener("click", () => {
                 localStorage.removeItem("customBGIMG");
-                $.jStorage.setItem("customBGIMG", '');
+                $.jStorage.set("customBGIMG", '');
                 document.body.style.backgroundImage = "none";
             });
             dbg.addEventListener("click", () => {
@@ -453,7 +453,7 @@ if ($.jStorage.get("customBGIMG")) {
         });
         rb.addEventListener("click", () => {
             localStorage.removeItem("customBGIMG");
-            $.jStorage.setItem("customBGIMG", '');
+            $.jStorage.set("customBGIMG", '');
             document.body.style.backgroundImage = "none";
         });
         dbg.addEventListener("click", () => {
