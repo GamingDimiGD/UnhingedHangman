8cb825ad6bf00a290ed576bb644a6b46651bc88a
GamingDimiGD <gamingdimigd@gmail.com>
2024-12-08 13:24:27 +0800

    修復預設主題載入不全bug

--- scripts/theme.js
+++ scripts/theme.js
@@ -94,7 +94,7 @@ let themeColorList = [
 
 let defaultTheme = []
 themeColorList.forEach(color => {
-    if(color.opacityHex) {
+    if (color.opacityHex) {
         defaultTheme.push({
             id: color.id,
             value: color.default + color.opacityHex
@@ -107,7 +107,7 @@ themeColorList.forEach(color => {
     })
 })
 
-if(!$.jStorage.get('theme')) $.jStorage.set('theme', defaultTheme);
+if (!$.jStorage.get('theme')) $.jStorage.set('theme', defaultTheme);
 
 $.jStorage.get('theme').forEach(color => setVar(color.id, color.value))
 
@@ -131,7 +131,7 @@ themeColorList.forEach(color => {
         button.style.background = getVar(color.id)
         input.value = color.default
         let themedata = $.jStorage.get('theme')
-        themedata.find(c =>  c.id === color.id).value = color.default
+        themedata.find(c => c.id === color.id).value = color.default
         $.jStorage.set('theme', themedata)
     }
     box.style.alignItems = 'center'
@@ -140,20 +140,20 @@ themeColorList.forEach(color => {
     cssPicker.innerText = 'CSS顏色代號'
     cssPicker.onclick = () => {
         let e = prompt('輸入CSS顏色代號(可使用半透明)')
-        if(!e) return;
+        if (!e) return;
         setVar(color.id, e)
         button.style.background = getVar(color.id)
         let themedata = $.jStorage.get('theme')
-        themedata.find(c =>  c.id === color.id).value = e
+        themedata.find(c => c.id === color.id).value = e
         $.jStorage.set('theme', themedata)
     }
     box.append(b, button, input, cssPicker, reset)
     $(".theme-colors").append(box)
-    input.addEventListener('change',  (e) => {
+    input.addEventListener('change', (e) => {
         setVar(color.id, e.target.value)
         button.style.background = getVar(color.id)
         let themedata = $.jStorage.get('theme')
-        themedata.find(c =>  c.id === color.id).value = e.target.value
+        themedata.find(c => c.id === color.id).value = e.target.value
         $.jStorage.set('theme', themedata)
     })
 })
@@ -181,45 +181,60 @@ let themeNames = [
     'snowy',
 ]
 
-themeNames.forEach(name => {
-    fetch(`../themes/data/${name}.json`)
-       .then(response => response.json())
-       .then(data => themes.push(data))
-})
-
 const presetList = $('.preset-list')
+const loadPresets = () => {
+    $(presetList).empty()
+    themes = [
+        {
+            id: 'classic',
+            name: '經典',
+            description: 'UnhingedHangman 的預設顏色!',
+            author: '遊戲迪米',
+            categories: ['綠色系', '亮色系'],
+            data: defaultTheme
+        },
+    ]
+    pushPreset(themes[0])
+    themeNames.forEach(name => {
+        fetch(`../themes/data/${name}.json`)
+            .then(response => response.json())
+            .then(theme => {
+                themes.push(theme)
+                pushPreset(theme)
+            })
+    })
+}
 
-setTimeout(() => {
-    themes.forEach(theme => {
-        let box = document.createElement('div')
-        box.classList.add('preset-item')
-        let img = document.createElement('img')
-        img.src = `../themes/thumbnail/${theme.id}.png`
-        let title = document.createElement('h2')
-        title.innerText = theme.name
-        let description = document.createElement('p')
-        description.innerHTML = theme.description
-        let category = document.createElement('b')
-        category.innerText = theme.categories.join(' ').trim()
-        let author = document.createElement('b')
-        author.innerText = "製作人: " + theme.author
-        let use =  document.createElement('button')
-        use.innerText = '套用'
-        use.onclick = () => {
-            theme.data.forEach(color => setVar(color.id, color.value))
-            $.jStorage.set('theme', theme.data)
-            $.jStorage.get('theme').forEach(t => {
-                document.querySelector(`.${t.id}-picker`).style.background =  t.value
-            });
-            showNotif('已套用', 1)
-        }
-        let info = document.createElement('div')
-        info.classList.add('preset-info')
-        info.append(title, category, author, description, use)
-        box.append(img, info)
-        presetList.append(box)
-    });
-}, 500)
+const pushPreset = (theme) => {
+    let box = document.createElement('div')
+    box.classList.add('preset-item')
+    let img = document.createElement('img')
+    img.src = `../themes/thumbnail/${theme.id}.png`
+    let title = document.createElement('h2')
+    title.innerText = theme.name
+    let description = document.createElement('p')
+    description.innerHTML = theme.description
+    let category = document.createElement('b')
+    category.innerText = theme.categories.join(' ').trim()
+    let author = document.createElement('b')
+    author.innerText = "製作人: " + theme.author
+    let use = document.createElement('button')
+    use.innerText = '套用'
+    use.onclick = () => {
+        theme.data.forEach(color => setVar(color.id, color.value))
+        $.jStorage.set('theme', theme.data)
+        $.jStorage.get('theme').forEach(t => {
+            document.querySelector(`.${t.id}-picker`).style.background = t.value
+        });
+        showNotif('已套用', 1)
+    }
+    let info = document.createElement('div')
+    info.classList.add('preset-info')
+    info.append(title, category, author, description, use)
+    box.append(img, info)
+    presetList.append(box)
+}
+
 
 const exportTheme = () => {
     let e = JSON.stringify($.jStorage.get('theme'));
@@ -235,7 +250,7 @@ const importTheme = (importSaveData) => {
     $.jStorage.set('theme', e)
     $.jStorage.get('theme').forEach(color => setVar(color.id, color.value))
     $.jStorage.get('theme').forEach(t => {
-        document.querySelector(`.${t.id}-picker`).style.background =  t.value
+        document.querySelector(`.${t.id}-picker`).style.background = t.value
     });
 };
 
